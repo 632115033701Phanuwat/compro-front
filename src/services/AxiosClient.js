2c4dbb37ac3001b653be9584e0d4dc05d963788d
Nattaparin <nattaparin455@gmail.com>
2022-10-27 17:04:21 +0700

    Prod-0.02

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://44.210.144.215:8999',
+  baseURL: 'http://3.83.149.64:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
